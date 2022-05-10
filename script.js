c80f40551d86cedd59c9ba6c12e29380d46027c6
Abidov-09 <105117121+Abidov-09@users.noreply.github.com>
2022-05-10 03:55:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// const surname = 'Abidov'
+// const name = 'Muhammad'
+// let age = 22
+
+// const pi = 3.14
+
+// surname ='jimi'
+// box = 7
+
+// console.log(surname)
+// console.log(age)
+// console.log(name)
+
+// let age = prompt('Yoshingz Nechida')
+// console.log(age)
+
+
+let name = prompt('Ismingiz Nima')
+
+let surname = prompt ('Familyangiz nima')
+
+let age = prompt('Yoshingiz Nechida')
+
+console.log(`My name is ${name} and my surname is ${surname} and I'm ${age} years old`);
